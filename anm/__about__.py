7b3c4cad8f98ea3bb414c53c2c4da0a39d26f623
Richard Strnad <git@strnad.ch>
2018-11-05 14:15:44 +0100

    Updated the version

--- anm/__about__.py
+++ anm/__about__.py
@@ -7,7 +7,7 @@ __title__ = 'anm'
 __summary__ = 'Asyncio Implementation of Netminko'
 __uri__ = 'https://github.com/richardstrnad/anm'
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 __author__ = 'Richard Strnad'
 __email__ = 'richard@strnad.ch'
